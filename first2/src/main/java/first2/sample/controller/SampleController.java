b2f1b16f68ee9bf98be42b16cdc45f3776b0c2d9
150628springboard <bark1@bark1-PC>
2015-08-13 21:51:55 +0900

    게시판 목록 구현

--- first2/src/main/java/first2/sample/controller/SampleController.java
+++ first2/src/main/java/first2/sample/controller/SampleController.java
@@ -1,11 +1,16 @@
 package first2.sample.controller;
 
+import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import first2.sample.service.SampleService;
 
 
 
@@ -13,11 +18,17 @@ import org.springframework.web.servlet.ModelAndView;
 public class SampleController {
 	Logger log = Logger.getLogger(this.getClass());
 	
+	@Resource(name="sampleService")
+    private SampleService sampleService;
+	
 	@RequestMapping(value="/sample/openSampleList.do")
     public ModelAndView openSampleList(Map<String,Object> commandMap) throws Exception{
-    	ModelAndView mv = new ModelAndView("");
+    	ModelAndView mv = new ModelAndView("/sample/boardList");
     	log.debug("인터셉터 테스트");
     	
+    	List<Map<String,Object>> list = sampleService.selectBoardList(commandMap);
+        mv.addObject("list", list);
+        
     	return mv;
     }
 }
