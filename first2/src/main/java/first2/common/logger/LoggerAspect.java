2837532a65a5ce372755e37ee91b2ec89fce18e3
150628springboard <bark1@bark1-PC>
2015-08-28 21:46:51 +0900

    서블릿 jsonview 수정 및 aop 설정

---
+++ first2/src/main/java/first2/common/logger/LoggerAspect.java
@@ -0,0 +1,31 @@
+package first2.common.logger;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+ 
+@Aspect
+public class LoggerAspect {
+    protected Log log = LogFactory.getLog(LoggerAspect.class);
+    static String name = "";
+    static String type = "";
+     
+    @Around("execution(* first2..controller.*Controller.*(..)) or execution(* first2..service.*Impl.*(..)) or execution(* first2..dao.*DAO.*(..))")
+    public Object logPrint(ProceedingJoinPoint joinPoint) throws Throwable {
+        type = joinPoint.getSignature().getDeclaringTypeName();
+         
+        if (type.indexOf("Controller") > -1) {
+            name = "Controller  \t:  ";
+        }
+        else if (type.indexOf("Service") > -1) {
+            name = "ServiceImpl  \t:  ";
+        }
+        else if (type.indexOf("DAO") > -1) {
+            name = "DAO  \t\t:  ";
+        }
+        log.debug(name + type + "." + joinPoint.getSignature().getName() + "()");
+        return joinPoint.proceed();
+    }
+}
